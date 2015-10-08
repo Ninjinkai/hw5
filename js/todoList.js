122264ada314b28c818b80dc8601201516fade15
Ninjinkai <nickpetty@hotmail.com>
2015-10-07 22:50:36 -0400

    Deleting, checking/unchecking tasks functional.  Cleaned up styling, CSS & JS code.

--- js/todoList.js
+++ js/todoList.js
@@ -16,47 +16,51 @@ $(function() {
     var items = $('li[class!=complete]').length; // Number of items in list
     $('#counter').text(items);                   // Added into counter circle
   }
-  updateCount();                                 // Call the function
-
-  // SETUP FORM FOR NEW ITEMS
-//  $newItemButton.show();                         // Show the button
-//  $newItemForm.hide();                           // Hide the form
-//  $('#showForm').on('click', function() {        // When click on add item button
-//    $newItemButton.hide();                       // Hide the button
-//    $newItemForm.show();                         // Show the form
-//  });
+    updateCount();                               // Call the function
     $newItemForm.show();                         // Show the form
 
   // ADDING A NEW LIST ITEM
   $newItemForm.on('submit', function(e) {       // When a new item is submitted
     e.preventDefault();                         // Prevent form being submitted
     var text = $('input:text').val();           // Get value of text input
-    $list.append('<li>' + text + '</li>');      // Add item to end of the list
+    $list.append('<li><div class="notDone">done?</div><div class="itemText">' + text + '</div><div class="trash">trash</div></li>');      // Add item to end of the list
     $('input:text').val('');                    // Empty the text input
     updateCount();                              // Update the count
   });
 
-  // CLICK HANDLING - USES DELEGATION ON <ul> ELEMENT
-  $list.on('click', 'li', function() {
-    var $this = $(this);               // Cache the element in a jQuery object
-    var complete = $this.hasClass('complete');  // Is item complete
+    $list.on('click', '.trash', function () {
+        var $this = $(this);
+        var $parent = $this.parent();
+        $parent.addClass('complete');
+        $parent.animate({
+            opacity: 0.0,
+            paddingLeft: '+=180'
+        }, 500, 'swing', function() {
+            $parent.remove();
+        });
+        updateCount();
+    });
 
-    if (complete === true) {           // Check if item is complete
-      $this.animate({                  // If so, animate opacity + padding
-        opacity: 0.0,
-        paddingLeft: '+=180'
-      }, 500, 'swing', function() {    // Use callback when animation completes
-        $this.remove();                // Then completely remove this item
-      });
-    } else {                           // Otherwise indicate it is complete
-        $this.addClass('complete');
-//      item = $this.text();             // Get the text from the list item
-//      $this.remove();                  // Remove the list item
-//      $list                            // Add back to end of list as complete
-//        .append('<li class=\"complete\">' + item + '</li>')
-//        .hide().fadeIn(300);           // Hide it so it can be faded in
-      updateCount();                   // Update the counter
-    }                                  // End of else option
-  });                                  // End of event handler
+    $list.on('click', '.notDone', function () {
+        var $this = $(this);
+        var $parent = $this.parent();
+        $this.removeClass('notDone');
+        $this.addClass('done').text('DONE!');
+        $parent.addClass('complete');
+        updateCount();
+    });
+
+    $list.on('click', '.done', function () {
+        var $this = $(this);
+        var $parent = $this.parent();
+        $this.removeClass('done');
+        $this.addClass('notDone').text('done?');
+        $parent.removeClass('complete');
+        updateCount();
+    });
+
+    $list.on('click', '.itemText', function () {
+
+    });
 
 });
