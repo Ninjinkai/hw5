2b70e5c77aa9fbc361f69892682a4f1b34500bcb
Ninjinkai <nickpetty@hotmail.com>
2015-10-14 22:55:49 -0400

    Further expansion of comments.

--- js/todoList.js
+++ js/todoList.js
@@ -8,14 +8,14 @@ $(function () {
     var $itemDescription = $('#itemDescription');
 
     //Load list from local storage
-    //NOT FUNCTIONAL
+    //NOT FUNCTIONAL: $list is not a JSON object
 //    var $listData = localStorage.getItem("listData");
 //    $list.append(JSON.parse($listData));
 
     //Save list to local storage
-    //NOT FUNCTIONAL
+    //NOT FUNCTIONAL: $list is not a JSON object
 //    function saveList() {
-//        localStorage.setItem("listData", $list.children());
+//        localStorage.setItem("listData", $list);
 //    }
 
     //Update incomplete items counter
@@ -29,14 +29,17 @@ $(function () {
     $newItemForm.show();
 
     //Clear editing controls, then reset input form
+    //This is called whenever a task is added or changed
     function removeEditMode() {
         var $editItem = $('#editItem');
         var $editParent = $editItem.parent();
+        //Remove editing state from list item and buttons
         $editItem.removeClass('editing');
         $editItem.removeAttr("id", "editItem");
         $editParent.removeClass('editing');
         $enterButton.val('add');
         $newItemForm.removeClass('editMode');
+        //Reset input box
         $itemDescription.val('');
         $itemDescription.attr("placeholder", "Add a task");
         updateCount();
@@ -47,11 +50,13 @@ $(function () {
         event.preventDefault();
         var $this = $(this);
         var $text = $itemDescription.val();
+        //Update edited item text
         if ($newItemForm.hasClass('editMode')) {
             var $editItem = $('#editItem');
             var $editParent = $editItem.parent();
             $editItem.text($text);
         } else {
+        //Add a new item
             $list.append('<li><div class="notDone" /><div class="itemText">' + $text + '</div><div class="trash" /></li>');
         }
         removeEditMode();
@@ -60,10 +65,12 @@ $(function () {
 
     //Delete an item when the trash icon is clicked
     $list.on('click', '.trash', function (event) {
+        //Prevent click from falling through to another element
         event.stopPropagation();
         var $this = $(this);
         var $parent = $this.parent();
         removeEditMode();
+        //Animate list item being removed
         $parent.addClass('complete');
         $parent.animate({
             opacity: 0.0,
@@ -77,10 +84,12 @@ $(function () {
 
     //Mark an item as done when the red box is clicked
     $list.on('click', '.notDone', function (event) {
+        //Prevent click from falling through to another element
         event.stopPropagation();
         var $this = $(this);
         var $parent = $this.parent();
         removeEditMode();
+        //Change item state from not done to done
         $this.removeClass('notDone');
         $this.addClass('done');
         $parent.addClass('complete');
@@ -90,10 +99,12 @@ $(function () {
 
     //Mark an item as not done when the green box is clicked
     $list.on('click', '.done', function (event) {
+        //Prevent click from falling through to another element
         event.stopPropagation();
         var $this = $(this);
         var $parent = $this.parent();
         removeEditMode();
+        //Change item state from done to not done
         $this.removeClass('done');
         $this.addClass('notDone');
         $parent.removeClass('complete');
@@ -107,12 +118,15 @@ $(function () {
         var $toEdit = $this.children('.itemText');
         var $text = $toEdit.text();
         removeEditMode();
+        //Add editing state to list item
         $toEdit.addClass('editing');
         $toEdit.attr("id", "editItem");
         $this.addClass('editing');
+        //Put item text into input box for editing
         $itemDescription.val($text);
         $itemDescription.attr("placeholder", "Edit task");
         $itemDescription.focus();
+        //Change button to show editing is active
         $enterButton.val('edit');
         $newItemForm.addClass('editMode');
     });
